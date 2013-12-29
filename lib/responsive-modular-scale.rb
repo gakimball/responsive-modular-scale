f84f0f8fd8ac9c66739ceccde0b788836b771935
Geoff Kimball <gakimball@bsu.edu>
2013-12-29 03:39:03 -0500

    Update gem version and date

--- lib/responsive-modular-scale.rb
+++ lib/responsive-modular-scale.rb
@@ -5,6 +5,6 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('responsive-modular-scale', :path => extension_path)
 
 module ResponsiveModularScale
-  VERSION = "0.0.1"
-  DATE = "2013-12-28"
+  VERSION = "0.0.2"
+  DATE = "2013-12-29"
 end
